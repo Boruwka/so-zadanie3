08af514b83c3fd8b6487e011d97a916a2d0b8d5c
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2022-05-09 08:37:36 +0200

    hello world from pm

---
+++ usr/src/minix/servers/pm/do_getlcapid.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "pm.h"
+#include "mproc.h" // ten tajemniczy include przyda się nam w zadaniu 3 ;)
+#include <limits.h> // w sumie to ten też
+
+int do_getlcapid(void)
+{
+    printf("Hello world from pm\n");
+    return 0;
+}

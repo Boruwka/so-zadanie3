4dd3b936bf4bfaebd5ab796d3c2eae4fa74de0ba
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2022-05-09 14:11:14 +0200

    przekazywanie argumentow z syscalla

--- usr/src/lib/libc/misc/getlcapid.c
+++ usr/src/lib/libc/misc/getlcapid.c
@@ -1,5 +1,6 @@
 #include <lib.h>
 #include <minix/rs.h>
+#include <stdio.h>
 
 int get_pm_endpt(endpoint_t *pt)
 {
@@ -12,10 +13,16 @@ pid_t getlcapid(pid_t pid1, pid_t pid2)
         message m;
         m.m_u32.data[0] = pid1;
         m.m_u32.data[1] = pid2;
+        printf("hw from funkcja biblioteczna, pidy to %d %d\n", pid1, pid2);
         if (get_pm_endpt(&pm_pt) != 0)
         {
                 errno = ENOSYS;
                 return -1;
         }
-        return (_syscall(pm_pt, PM_GETLCAPID, &m));
+        if (_syscall(pm_pt, PM_GETLCAPID, &m) != 0)
+        {
+            return -1; // errno się ustawiło w syscallu
+        }
+        printf("zwrocony pid to %d\n", m.m_u32.data[0]);
+        return m.m_u32.data[0];
 }
--- usr/src/minix/servers/pm/do_getlcapid.c
+++ usr/src/minix/servers/pm/do_getlcapid.c
@@ -8,5 +8,6 @@ int do_getlcapid(void)
     pid_t pid1 = m_in.m_u32.data[0];
     pid_t pid2 = m_in.m_u32.data[1];
     printf("Hello world from pm, pids are %d and %d\n", pid1, pid2);
+    mp->mp_reply.m_u32.data[0] = pid1 + pid2; // na razie 
     return 0;
 }
